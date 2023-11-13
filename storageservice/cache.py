6ec45e1245055ed23ac09f6c0fd5d2eb749eece2
aditya.singh <aditya.singh@tataiq.com>
2023-11-13 09:15:36 +0000

    error resolved

--- storageservice/cache.py
+++ storageservice/cache.py
@@ -8,27 +8,32 @@ print(configdata)
 api = configdata["apis"]
 kafka=configdata["kafka"]
 topic=configdata["event_topic"][0]
-if configdata:
+camera_group=[]
+customer=[]
+subsite=[]
+location=[]
+
+if configdata["readConfig"]:
     try:
         customer = configdata["config"]["customer"]
     except RuntimeWarning as ex:
         print("Customer Ids Not found: ", ex)
-        customer = None
+        customer = []
     try:
         subsite = configdata["config"]["subsite"]
     except RuntimeWarning as ex:
         print("Subsite Ids Not Found: ", ex)
-        subsite = None
+        subsite = []
     try:
         location = configdata["config"]["location"]
     except RuntimeWarning as ex:
         print("Location not found: ", ex)
-        location = None
+        location = []
     try:
         camera_group = configdata["config"]["group"]
     except RuntimeWarning as ex:
         print("Camera Group Not Found: ", ex)
-        camera_group = None
+        camera_group = []
 
 cs = Caching(api, camera_group, customer, location, subsite)
 cs.checkEvents(kafka,topic)
